c753b9e8fc4ba89b26f15a5b6d2e68d45aa627c5
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-05-14 02:42:26 -0700

    fix: no markdown plugins for now

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,10 +1,9 @@
 import { Remark } from "react-remark";
-import gfm from "remark-gfm";
 
 type MarkdownProps = {
   children: string;
 };
 
 export default function Markdown({ children }: MarkdownProps) {
-  return <Remark remarkPlugins={[gfm as any]}>{children}</Remark>;
+  return <Remark>{children}</Remark>;
 }
